c2723c632bbc59c7bb5eaadac5d499b7c9462c5a
Sixten Otto <sixten@resultsdirect.com>
2017-08-01 15:19:26 -0700

    rename pasteDelegate for iOS 11 compatibility

--- JSQMessagesViewController/Views/JSQMessagesComposerTextView.m
+++ JSQMessagesViewController/Views/JSQMessagesComposerTextView.m
@@ -158,7 +158,7 @@
 
 - (void)paste:(id)sender
 {
-    if (!self.pasteDelegate || [self.pasteDelegate composerTextView:self shouldPasteWithSender:sender]) {
+    if (!self.pastingDelegate || [self.pastingDelegate composerTextView:self shouldPasteWithSender:sender]) {
         [super paste:sender];
     }
 }
